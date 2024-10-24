4b8ea03176cc7b9fc3a4bb51b37065467ab78c3a
Jesús Atacho <jesusdavid1510@gmail.com>
2024-10-24 14:55:39 -0600

    Comitiado

---
+++ servidor.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Permitir CORS para desarrollos en distintos dominios
+
+@app.route('/')
+def index():
+    return "Servidor WebSocket con Flask está funcionando."
+
+# Evento para recibir y enviar datos en tiempo real
+@socketio.on('connect')
+def handle_connect():
+    print("Cliente conectado.")
+    emit('message', {'data': 'Bienvenido al servidor WebSocket'})
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("Cliente desconectado.")
+
+@socketio.on('new_data')
+def handle_new_data(data):
+    print(f"Datos recibidos: {data}")
+    emit('response', {'data': 'Datos recibidos correctamente'})
+
+if __name__ == '__main__':
+    socketio.run(app, host='192.168.137.180', port=3000)
